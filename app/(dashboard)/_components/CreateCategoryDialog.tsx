f18663f538b9fab479c581c1ec44106ec7ee0e3a
Dan <daniel.smirnov17@gmail.com>
2024-05-14 00:24:54 +0100

    added create new category form

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -12,10 +12,30 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
-import { PlusSquare } from "lucide-react";
-import React, { ReactNode, useState } from "react";
+import { CircleOff, PlusSquare } from "lucide-react";
+import React, { ReactNode, useCallback, useState } from "react";
 import { Category } from "@prisma/client";
 import { cn } from "@/lib/utils";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+} from "@/components/ui/form";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateCategorySchema, CreateCategorySchemaType } from "@/schema/categories";
+import { useTheme } from "next-themes";
 
 interface Props {
     type: TransactionType;
@@ -25,6 +45,18 @@ interface Props {
 
 function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
     const [open, setOpen] = useState(false);
+
+    const theme = useTheme();
+
+    const form = useForm<CreateCategorySchemaType>({
+        resolver: zodResolver(CreateCategorySchema),
+        defaultValues: {
+            type,
+        },
+    });
+
+    const onSubmit = useCallback((values: CreateCategorySchemaType) => { }, []);
+
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
@@ -58,6 +90,74 @@ function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
                         Categories are used to group your transactions
                     </DialogDescription>
                 </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Category" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how your category will appear in the app
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="icon"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Icon</FormLabel>
+                                    <FormControl>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className="h-[100px] w-full"
+                                                >
+                                                    {form.watch("icon") ? (
+                                                        <div className="flex flex-col items-center gap-2">
+                                                            <span className="text-5xl" role="img">
+                                                                {field.value}
+                                                            </span>
+                                                            <p className="text-xs text-muted-foreground">
+                                                                Click to change
+                                                            </p>
+                                                        </div>
+                                                    ) : (
+                                                        <div className="flex flex-col items-center gap-2">
+                                                            <CircleOff className="h-[48px] w-[48px]" />
+                                                            <p className="text-xs text-muted-foreground">
+                                                                Click to select
+                                                            </p>
+                                                        </div>
+                                                    )}
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-full">
+                                                <Picker
+                                                    data={data}
+                                                    theme={theme.resolvedTheme}
+                                                    onEmojiSelect={(emoji: { native: string }) => {
+                                                        field.onChange(emoji.native);
+                                                    }}
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how your category will appear in the app
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
                 <DialogFooter>
                     <DialogClose asChild>
                         <Button
