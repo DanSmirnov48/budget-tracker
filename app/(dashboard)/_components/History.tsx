587d8a2b7139a91f00d470918161c67465e4b2dc
Dan <daniel.smirnov17@gmail.com>
2024-06-10 21:51:45 +0100

    added ToolTip to the History Graph

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -4,7 +4,7 @@ import { GetFormatterForCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import HistoryPeriodSelector from "./HistoryPeriodSelector";
 import { useQuery } from "@tanstack/react-query";
@@ -14,9 +14,12 @@ import {
     BarChart,
     CartesianGrid,
     ResponsiveContainer,
+    Tooltip,
     XAxis,
     YAxis,
 } from "recharts";
+import CountUp from "react-countup";
+import { cn } from "@/lib/utils";
 
 function History({ userSettings }: { userSettings: UserSettings }) {
     const [timeframe, setTimeframe] = useState<Timeframe>("month");
@@ -149,6 +152,12 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                                         radius={4}
                                         className="cursor-pointer"
                                     />
+                                    <Tooltip
+                                        cursor={{ opacity: 0.1 }}
+                                        content={(props) => (
+                                            <CustomTooltip formatter={formatter} {...props} />
+                                        )}
+                                    />
                                 </BarChart>
                             </ResponsiveContainer>
                         )}
@@ -164,7 +173,77 @@ function History({ userSettings }: { userSettings: UserSettings }) {
                 </CardContent>
             </Card>
         </div>
-    )
+    );
 }
 
 export default History;
+
+function CustomTooltip({ active, payload, formatter }: any) {
+    if (!active || !payload || payload.length === 0) return null;
+
+    const data = payload[0].payload;
+    const { expense, income } = data;
+
+    return (
+        <div className="min-w-[300px] rounded border bg-background p-4">
+            <TooltipRow
+                formatter={formatter}
+                label="Expense"
+                value={expense}
+                bgColor="bg-red-500"
+                textColor="text-red-500"
+            />
+            <TooltipRow
+                formatter={formatter}
+                label="Income"
+                value={income}
+                bgColor="bg-emerald-500"
+                textColor="text-emerald-500"
+            />
+            <TooltipRow
+                formatter={formatter}
+                label="Balance"
+                value={income - expense}
+                bgColor="bg-gray-100"
+                textColor="text-foreground"
+            />
+        </div>
+    );
+}
+
+function TooltipRow({
+    label,
+    value,
+    bgColor,
+    textColor,
+    formatter,
+}: {
+    label: string;
+    textColor: string;
+    bgColor: string;
+    value: number;
+    formatter: Intl.NumberFormat;
+}) {
+    const formattingFn = useCallback((value: number) => {
+        return formatter.format(value);
+    }, [formatter]);
+
+    return (
+        <div className="flex items-center gap-2">
+            <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+            <div className="flex w-full justify-between">
+                <p className="text-sm text-muted-foreground">{label}</p>
+                <div className={cn("text-sm font-bold", textColor)}>
+                    <CountUp
+                        duration={0.5}
+                        preserveValue
+                        end={value}
+                        decimals={0}
+                        formattingFn={formattingFn}
+                        className="text-sm"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
